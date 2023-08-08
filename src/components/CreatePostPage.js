3f4571eb704c1bb8cfe4f0fac89865fb047e656b
avaneeshdayadav <avaneeshdyadav@gmail.com>
2023-08-08 14:51:28 +0530

    Creat Post basic beautification done

--- src/components/CreatePostPage.js
+++ src/components/CreatePostPage.js
@@ -1,120 +1,192 @@
 import React, { useState } from "react";
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
+import CloseButton from 'react-bootstrap/CloseButton';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import backgroundImage from '../img/icons8-delete.svg';
+import "../css/create_post.css";
+import bubbleSvg from "../img/svgtest.svg"
 
 
 const CreatePostPage = () => {
 
-  const [ingredients, setIngredients] = useState([{ id: 1, content: 'Add step Description' }]);
+  const [steps, setSteps] = useState([{ id: 1, content: 'Add step Description' }]);
+  const [ingridients, setIngridients] = useState([]);
+  const [ingridientInputValue, setIngridientInputValue] = useState('');
 
   const randomNumberGenerator = () => {
     return Math.floor(Math.random() * 1000000000000); // Generates a random 12-digit number
   };
 
   const handleAddStepsClick = () => {
-    const newIngredientId = randomNumberGenerator();
-    setIngredients([...ingredients, { id: newIngredientId, content: 'Add step Description' }]);
+    const newStep = randomNumberGenerator();
+    setSteps([...steps, { id: newStep, content: 'Add step Description' }]);
   };
 
   const handleRemoveStepClick = (id) => {
-    setIngredients(ingredients.filter((ingredient) => ingredient.id !== id));
+    setSteps(steps.filter((step) => step.id !== id));
   };
 
-  const handleIngredientChange = (index, newText) => {
-    const updatedIngredients = [...ingredients];
-    updatedIngredients[index].content = newText;
-    setIngredients(updatedIngredients);
+  const handleRemoveBadgeClick = (id) => {
+    setIngridients(ingridients.filter((ingridient) => ingridient.id !== id));
+  };
+
+  const handleStepChange = (index, newText) => {
+    const updatedSteps = [...steps];
+    updatedSteps[index].content = newText;
+    setSteps(updatedSteps);
   };
 
   const autoAdjustTextArea = (event, index) => {
     const textarea = event.target;
     textarea.style.height = 'auto';
     textarea.style.height = `${textarea.scrollHeight}px`;
-    handleIngredientChange(index, textarea.value);
+    handleStepChange(index, textarea.value);
   };
+
+  const addIngridient = (content) => {
+    const newingridient = Math.floor(Math.random() * 10000000000);;
+    setIngridients([...ingridients, { id: newingridient, content: content }]);
+    setIngridientInputValue('');
+  }
+
+  const handleAddIngridientButtonClick = () => {
+    addIngridient(ingridientInputValue);
+  }
+
+  const handleIngridientInputChange = (e) => {
+    if (e.key === 'Enter') {
+      addIngridient(ingridientInputValue);
+      e.target.value = '';
+    }
+    setIngridientInputValue(e.target.value);
+  }
 
   return (
     <>
       <h3>Create a post page</h3>
-      <Container>
-        <FloatingLabel
-          controlId="floatingInput"
-          label="Recipe Title"
-          className="mb-3"
-        >
-          <Form.Control type="text" placeholder="" />
-        </FloatingLabel><br></br>
-
-        <FloatingLabel controlId="floatingTextarea1" label="Description">
-          <Form.Control
-            as="textarea"
-            placeholder=""
-            style={{ height: '100px' }}
-          />
-        </FloatingLabel><br></br>
-
-        <FloatingLabel controlId="floatingSelect" label="Category">
-          <Form.Select aria-label="Floating label select example">
-            <option>None</option>
-            <option value="veg">Veg</option>
-            <option value="green-vegies">Green Vegies</option>
-            <option value="sweets">Sweets</option>
-            <option value="non-veg">Non Veg</option>
-            <option value="salads">Salads</option>
-            <option value="rice-biryani">Rice & Biryani</option>
-            <option value="street-food">Street Food</option>
-            <option value="franchise">Franchise</option>
-            <option value="south-indian">South Indian</option>
-          </Form.Select>
-        </FloatingLabel><br></br>
-
-        <div><h3>Add Steps:</h3></div>
-
-        <div id='mydiv' style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-          {ingredients.map((ingredient, index) => (
-            <div key={`ingredient_${ingredient.id}`} style={{ display: 'flex', alignItems: 'center' }}>
+      <div className="create-post-outer-container">
+        <Container>
+          <div className="create-post-divs" >
+            <div><h3 style={{ color: 'gray' }}>Basic info:</h3></div>
+            <div style={{ padding: "1%" }}>
               <FloatingLabel
-                controlId={`floatingTextarea${ingredient.id}`}
-                label={`Step ${index + 1}`}
-                className='mt-2'
+                controlId="floatingInput"
+                label="Recipe Title"
+                className="mb-3"
               >
+                <Form.Control type="text" placeholder="" />
+              </FloatingLabel>
+            </div>
+
+            <div style={{ padding: "1%" }}>
+              <FloatingLabel controlId="floatingSelect" label="Category">
+                <Form.Select aria-label="Floating label select example">
+                  <option>None</option>
+                  <option value="veg">Veg</option>
+                  <option value="green-vegies">Green Vegies</option>
+                  <option value="sweets">Sweets</option>
+                  <option value="non-veg">Non Veg</option>
+                  <option value="salads">Salads</option>
+                  <option value="rice-biryani">Rice & Biryani</option>
+                  <option value="street-food">Street Food</option>
+                  <option value="franchise">Franchise</option>
+                  <option value="south-indian">South Indian</option>
+                </Form.Select>
+              </FloatingLabel>
+            </div>
+
+            <div style={{ padding: "1%" }}>
+              <FloatingLabel controlId="floatingTextarea1" label="Description">
                 <Form.Control
                   as="textarea"
-                  rows={1}
-                  value={ingredient.content}
-                  onChange={(e) => autoAdjustTextArea(e, index)}
-                  onBlur={(e) => autoAdjustTextArea(e, index)}
-                  style={{ resize: 'none', overflow: 'hidden', minHeight: '37px', borderRadius: '5px'}}
+                  placeholder=""
+                  style={{ height: '100px' }}
                 />
               </FloatingLabel>
-              <Button  onClick={() => handleRemoveStepClick(ingredient.id)}
-              style={{
-                backgroundImage: `url(${backgroundImage})`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat',
-                backgroundColor: 'white',
-                border: '0px',
-                width: '5vh',
-                height: '5vh',
-              }}
-              ></Button>
+              <div style={{ padding: "1%" }}>
+                <FloatingLabel>
+                  <Form.Check // prettier-ignore
+                    type="switch"
+                    id="custom-switch"
+                    label="Public"
+                  />
+                </FloatingLabel>
+              </div>
             </div>
-          ))}
-          <Button variant='warning' onClick={handleAddStepsClick}>Add Step</Button>
-        </div><br></br>
+          </div>
 
-        <FloatingLabel>
-          <Form.Check // prettier-ignore
-            type="switch"
-            id="custom-switch"
-            label="Public"
-          />
-        </FloatingLabel>
-      </Container>
+          <div className="create-post-divs" style={{ marginTop: "4%" }}>
+            <div><h3 style={{ color: 'gray' }}>Add Steps:</h3></div>
+
+            <div id='mydiv' style={{ padding: "1%" }}>
+              {steps.map((step, index) => (
+                <div key={`step_${step.id}`}>
+                  <FloatingLabel
+                    controlId={`floatingTextarea${step.id}`}
+                    label={`Step ${index + 1}`}
+                    className='mt-2'
+                  >
+                    <Form.Control
+                      as="textarea"
+                      rows={1}
+                      value={step.content}
+                      onChange={(e) => autoAdjustTextArea(e, index)}
+                      onBlur={(e) => autoAdjustTextArea(e, index)}
+                      style={{ resize: 'none', overflow: 'hidden', minHeight: '37px', borderRadius: '5px' }}
+                    />
+                  </FloatingLabel>
+                  <Button onClick={() => handleRemoveStepClick(step.id)}
+                    style={{
+                      backgroundImage: `url(${backgroundImage})`,
+                      backgroundSize: 'cover',
+                      backgroundPosition: 'center',
+                      backgroundRepeat: 'no-repeat',
+                      backgroundColor: 'white',
+                      border: '0px',
+                      width: '5vh',
+                      height: '5vh',
+                    }}
+                  ></Button>
+                </div>
+              ))}
+              <div style={{ padding: "1%" }}><Button className="create-post-button" onClick={handleAddStepsClick} style={{ color: 'white', border: "0px" }}>Add Step</Button></div>
+            </div>
+          </div>
+
+          <div className="create-post-divs" style={{ marginTop: "4%" }}>
+            <div><h3 style={{ color: 'gray' }}>Add ingredient:</h3></div>
+            <div style={{ padding: "1%" }}>
+              <div style={{ width: "100%", display: "flex", justifyContent: "space-evenly" }}>
+                <div style={{ width: "80%", padding: "1%" }}><Form.Control type="text" placeholder="Normal text" value={ingridientInputValue} onChange={handleIngridientInputChange} onKeyDown={handleIngridientInputChange} /></div>
+                <div style={{ width: "20%", padding: "1%" }}><Button className="create-post-button" onClick={handleAddIngridientButtonClick} style={{ color: 'white', border: "0px" }}>Add</Button></div>
+              </div>
+            </div>
+            <div style={{ padding: "1%" }}>
+              {ingridients.map((ingridient) => {
+                return (
+                  <>
+                    <div key={`ingridient_${ingridient.id}`} style={{ maxWidth: '100%', overflow: 'scroll', margin: '3px', backgroundColor: 'orange', alignItems: "center", padding: "0.5% 1% 1% 3%", borderRadius: "25px", display: 'inline-block' }}>
+                      <div style={{ backgroundColor: "orange", color: "white", display: "inline-block" }}>
+                        <span>{ingridient.content}</span>
+                      </div>&nbsp;&nbsp;
+                      <div style={{ display: "inline-block" }}>
+                        <CloseButton variant="white" onClick={() => { handleRemoveBadgeClick(ingridient.id) }} />&nbsp;&nbsp;
+                      </div>
+                    </div>
+                  </>
+                )
+              })}
+            </div>
+          </div>
+
+          <div className="create-post-divs" style={{ marginTop: "4%" }}>
+            <Button className="create-post-button" style={{ color: 'white', border: '0px' }}>Create Recipe Post</Button>
+          </div>
+        </Container>
+      </div>
+
     </>
   )
 }
