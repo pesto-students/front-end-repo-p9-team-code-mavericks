f7e7aeea1c66a339a71bb3a03b1f63d8dbe1762b
pranavkashyap2606 <pprraannaavv123@gmail.com>
2023-08-27 19:45:18 +0530

    Beautifying the Cards

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,7 +1,7 @@
 import Cookies from "js-cookie";
 import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+// import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import card_img from '../img/recipe_login_wallpaper.jpg';
 import '../css/homepage.css';
@@ -9,6 +9,19 @@ import unlikeImg from '../img/icons8-heart-unliked.png';
 import likeImg from '../img/icons8-heart-liked.png';
 import bookmarkImg from '../img/icons8-bookmarked.png';
 import unbookmarkImg from '../img/icons8-unbookmark.png'
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+
 
 const RecipeCard = (props) => {
   const [isBookmarked, setIsBookmarked] = useState(props.feed.bookmarked);
@@ -65,12 +78,96 @@ const RecipeCard = (props) => {
     console.log('view '+window.location.href);
     window.location.href = '/post/'+props.feed._id;
   }
+
+  function capitalizeFirstLetter(word) {
+    if (typeof word === 'undefined') {
+      return ''; // Handle empty string
+    }
+    if (word.length === 0) {
+      return ''; // Handle empty string
+    }
+    return word[0].toUpperCase();
+  }
+
   const src_var = process.env.REACT_APP_WEBSITE_URL+"/uploads/ca2080db-afce-4f0a-9869-dda96f405ca2.png"
   console.log(src_var);
   return (
     <Container className="recipe-card-container">
-      <div className="recipe-card-inner-div">
-        <div className='recipe-card-img-div'>
+      {/* <div className="recipe-card-inner-div"> */}
+      <Card sx={{ maxWidth: '100%' }}>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+              {capitalizeFirstLetter(props.feed.author_username)}
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="bookmark">
+              {
+              (isBookmarked) ?
+                <div style={{ cursor: 'pointer', width: '4vh', height: '4vh', borderRadius: '25px', }} onClick={toogleBookmark}>
+                  <img src={bookmarkImg} alt='like' style={{ maxHeight: '100%', maxWidth: '100%', height: '100%', width: '100%' }} />
+                </div> :
+                <div style={{ cursor: 'pointer', width: '4vh', height: '4vh', borderRadius: '25px', }} onClick={toogleBookmark}>
+                  <img src={unbookmarkImg} alt='like' style={{ maxHeight: '100%', maxWidth: '100%', height: '100%', width: '100%' }} />
+                </div>
+            }
+            </IconButton>
+          }
+          title={props.feed.recipe_title}
+          subheader={"by " +props.feed.author_username}
+        />
+        <CardMedia
+          component="img"
+          height="auto"
+          image={src_var}
+          alt="Paella dish"
+        />
+        
+        <CardContent>
+        <div style={{display: 'flex', justifyContent: 'space-between'}}>
+        <div style={{display: 'flex', justifyContent: 'space-around'}}>
+        <div style={{fontSize: '0.8rem', color: 'blue'}}>10 views</div>&nbsp;
+        <div style={{fontSize: '1rem', color: 'blue'}}> | </div>&nbsp;
+        <div style={{fontSize: '0.8rem', color: 'blue'}}>{likesCnt + ' Likes'}</div>
+        </div>
+        <div style={{fontSize: '0.6rem', color: 'blue'}}>26 July 2023</div>
+        </div>
+          <Typography variant="body2" color="text.secondary">
+            {props.feed.recipe_description}
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            {
+              (!isLiked) ?
+                <>
+                  <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <div style={{ cursor: 'pointer', width: '4vh', height: '4vh', borderRadius: '25px', }} onClick={toogleLike}>
+                      <img src={unlikeImg} alt='like' style={{ maxHeight: '100%', maxWidth: '100%', height: '100%', width: '100%' }} />
+                    </div>
+                    {/* <div></div>
+                    <div style={{ color: 'gray', fontSize: '0.8rem' }}>{likesCnt} Likes.</div> */}
+                  </div>
+                </>
+                :
+                <>
+                  <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <div style={{ cursor: 'pointer', width: '4vh', height: '4vh', borderRadius: '25px', }} onClick={toogleLike}>
+                      <img src={likeImg} alt='like' style={{ maxHeight: '100%', maxWidth: '100%', height: '100%', width: '100%' }} />
+                    </div>
+                    {/* <div></div>
+                    <div style={{ color: 'gray', fontSize: '0.8rem' }}>{likesCnt} Likes.</div> */}
+                  </div>
+                </>
+            }
+          </IconButton>
+          <IconButton aria-label="viewMore">
+          <div onClick={viewDetailedRecipe} ><Button style={{ backgroundColor: 'orange', color: 'white', borderRadius: '10px', border: '0px' }}>View</Button></div>
+          </IconButton>
+        </CardActions>
+      </Card>
+        {/* <div className='recipe-card-img-div'>
           <img className="recipe-card-img" src={src_var} ></img>
         </div>
         <div className='recipe-card-content-div' >
@@ -99,8 +196,8 @@ const RecipeCard = (props) => {
                 </>
             }
             <div onClick={viewDetailedRecipe} ><Button style={{ backgroundColor: 'orange', color: 'white', borderRadius: '10px', border: '0px' }}>View</Button></div>
-          </div>
-          <div className="recipe-card-bookmark-icon">
+          </div> */}
+          {/* <div className="recipe-card-bookmark-icon">
             {
               (isBookmarked) ?
                 <div style={{ cursor: 'pointer', width: '4vh', height: '4vh', borderRadius: '25px', }} onClick={toogleBookmark}>
@@ -111,9 +208,9 @@ const RecipeCard = (props) => {
                 </div>
             }
           </div>
-          <div className='recipe-card-author-tag'><small>Author: <i style={{ color: 'blue' }}>{props.feed.author_username}</i></small></div>
-        </div>
-      </div>
+          <div className='recipe-card-author-tag'><small>Author: <i style={{ color: 'blue' }}>{props.feed.author_username}</i></small></div> */}
+        {/* </div> */}
+      {/* </div> */}
     </Container>
   );
 };
